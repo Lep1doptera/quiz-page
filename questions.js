9e1f20064ce6dbddd35f23fc24faa7a56a176019
Mandy <ferguson.am19@gmail.com>
2024-10-05 17:39:43 +1000

    Added React-app and started customizing

---
+++ questions.js
@@ -0,0 +1,99 @@
+
+
+// Quiz Questions
+ export const myQuestions = [
+    {
+      question: "What is the name of the main character in Alien",
+      answers: {
+        a: "Ellen Ripley",
+        b: "Pennywise",
+        c: "Freddy Krueger",
+      },
+      correctAnswer: "Ellen Ripley",
+    },
+    {
+      question: "What year did the third Alien film release?",
+      answers: {
+        a: "1999",
+        b: "1992",
+        c: "1983",
+      },
+      correctAnswer: "1992",
+    },
+    {
+      question: "What is the name of the ship in the first Alien film",
+      answers: {
+        a: "Serenity",
+        b: "Nostromo",
+        c: "Millennium Falcon",
+      },
+      correctAnswer: "Nostromo",
+    },
+    {
+      question:
+        "Complete this quote from Aliens; 'You know, Burke, I dont know which species is worse. You dont see them f*****g each other over for a goddamn _______.'",
+      answers: {
+        a: "Percentage",
+        b: "Snack",
+        c: "Profit",
+      },
+      correctAnswer: "Percentage",
+    },
+    {
+      question: "Who was the first to die in Alien",
+      answers: {
+        a: "Parker",
+        b: "Brett",
+        c: "Kane",
+      },
+      correctAnswer: "Kane",
+    },
+    {
+      question: "Who directed Aliens?",
+      answers: {
+        a: "Tim Burton",
+        b: "James Cameron",
+        c: "Ridley Scott",
+      },
+      correctAnswer: "James Cameron",
+    },
+    {
+      question: "What is the androids name in Alien 4",
+      answers: {
+        a: "David",
+        b: "Bishop",
+        c: "Call",
+      },
+      correctAnswer: "Call",
+    },
+    {
+      question: "Which character did Bill Paxton play?",
+      answers: {
+        a: "Hicks",
+        b: "Hudson",
+        c: "Gorman",
+      },
+      correctAnswer: "Hudson",
+    },
+    {
+      question: "What gift did Ellen give to the Queen in Alien 4",
+      answers: {
+        a: "Immortality",
+        b: "Birth",
+        c: "Speech",
+      },
+      correctAnswer: "Birth",
+    },
+    {
+      question: "Who designed the creature Xenomorph?",
+      answers: {
+        a: "Claude Monet",
+        b: "H.R.Giger",
+        c: "Pueblo Picos",
+      },
+      correctAnswer: "H.R.Giger",
+    },
+  ];
+
+
+
---
+++ src/App.js
@@ -0,0 +1,8 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <button id="submit">Start</button>;
+}
+
+export default App;
